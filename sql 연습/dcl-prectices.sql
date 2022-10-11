4bd87a79edc4b23a82a0d4f86ada0d2d3faa8e14
tjdgns0029 <whtjdgns0029@naver.com>
2022-10-11 17:23:31 +0900

    dcl연습tool

--- sql 연습/dcl-prectices.sql
+++ sql 연습/dcl-prectices.sql
@@ -5,3 +5,13 @@ create database webdb;
 show databases;
 
 -- db 사용자 생성
+create user 'webdb'@'localhost' identified by 'webdb';
+
+-- db 사용자 삭제
+drop user 'webdb'@'locahost'; 
+
+-- 권한 부여
+grant all privileges on webdb.* to 'webdb'@'localhost';
+
+-- 권한 정보 재로딩
+flush privileges;
