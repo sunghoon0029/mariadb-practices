5f67f82b677eb1dc9e7d2464a7cbe2852dbe22b0
sunghoon0029 <114986321+sunghoon0029@users.noreply.github.com>
2022-10-23 20:57:07 +0900

    Update model_order.py

--- mysqlclient-practices/bookmall/model/model_order.py
+++ mysqlclient-practices/bookmall/model/model_order.py
@@ -1,1 +1,52 @@
+from MySQLdb import connect, OperationalError
+from MySQLdb.cursors import DictCursor
 
+def conn():
+    db = connect(
+        user='bookmall',
+        password='bookmall',
+        host='127.0.0.1',
+        port=3306,
+        db='bookmall',
+        charset='utf8')
+    return db
+
+def findall():
+    try:
+        db = conn()
+
+        cursor = db.cursor(DictCursor)
+
+        sql = 'select orders_no, price, address from orders order by orders_no asc'
+        cursor.execute(sql)
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        db.close()
+
+        return results
+
+    except OperationalError as e:
+        print(f'에러: {e}')
+
+
+
+def insert(price, address):
+    try:
+        db = conn()
+
+        cursor = db.cursor()
+
+        sql = 'insert into cartegory values(null, %s, %s)'
+        count = cursor.execute(sql, (price, address))
+
+        db.commit()
+
+        cursor.close()
+        db.close()
+
+        return count == 1
+
+    except OperationalError as e:
+        print(f'에러: {e}')
