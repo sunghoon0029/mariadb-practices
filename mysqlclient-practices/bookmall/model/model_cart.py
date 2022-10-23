32abeff66ec85c4711848a0b799302a2ca5c1f69
sunghoon0029 <114986321+sunghoon0029@users.noreply.github.com>
2022-10-23 20:49:38 +0900

    Update model_cart.py

--- mysqlclient-practices/bookmall/model/model_cart.py
+++ mysqlclient-practices/bookmall/model/model_cart.py
@@ -1,1 +1,52 @@
+from MySQLdb import connect, OperationalError
+from MySQLdb.cursors import DictCursor
 
+def conn():
+    db = connect(
+        user='bookmall',
+        password='bookmall',
+        host='127.0.0.1',
+        port=3306,
+        db='bookmall',
+        charset='utf8')
+    return db
+
+def findall():
+    try:
+        db = conn()
+
+        cursor = db.cursor(DictCursor)
+
+        sql = 'select cart_no, count from cart order by cart_no asc'
+        cursor.execute(sql)
+
+        results = cursor.fetchall()
+
+        cursor.close()
+        db.close()
+
+        return results
+
+    except OperationalError as e:
+        print(f'에러: {e}')
+
+
+
+def insert(cartcount):
+    try:
+        db = conn()
+
+        cursor = db.cursor()
+
+        sql = 'insert into cartegory values(null, %s)'
+        count = cursor.execute(sql, (cartcount, ))
+
+        db.commit()
+
+        cursor.close()
+        db.close()
+
+        return count == 1
+
+    except OperationalError as e:
+        print(f'에러: {e}')
