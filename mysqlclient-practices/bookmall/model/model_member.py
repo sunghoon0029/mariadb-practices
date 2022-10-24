55811474387562f17ee2cd2048f8ce6c6d7f6f45
sunghoon0029 <114986321+sunghoon0029@users.noreply.github.com>
2022-10-24 13:18:47 +0900

    Update model_member.py

--- mysqlclient-practices/bookmall/model/model_member.py
+++ mysqlclient-practices/bookmall/model/model_member.py
@@ -19,7 +19,8 @@ def findall():
 
         cursor = db.cursor(DictCursor)
 
-        sql = 'select member_name, tel, email, password from member'
+        sql = 'select member_no, member_name, tel, email, password from member'
+
         cursor.execute(sql)
 
         results = cursor.fetchall()
@@ -34,14 +35,14 @@ def findall():
 
 # -----------------------------------------------------
 
-def insert(membername, tel, email, password):
+def insert(member_name, tel, email, password):
     try:
         db = conn()
 
         cursor = db.cursor()
 
-        sql = 'insert into cartegory values(null, %s, %s, %s, %s)'
-        count = cursor.execute(sql, (membername, tel, email, password))
+        sql = 'insert into member values(null, %s, %s, %s, %s)'
+        count = cursor.execute(sql, (member_name, tel, email, password))
 
         db.commit()
 
